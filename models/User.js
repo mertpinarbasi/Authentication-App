d4b5260e2276b0c4162f31d67c5247be0db6690f
mertpinarbasi <mpinarbasi35@gmail.com>
2021-10-02 22:15:52 +0300

    Email Verification

--- models/User.js
+++ models/User.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	surname: { type: String, required: true },
 	email: { type: mongoose.SchemaTypes.Email, required: true, unique: true },
-	password: { type: String, required: true }
+	password: { type: String, required: true },
+	isVerified: { type: Boolean, required: true },
+	emailToken: { type: String, required: true }
 });
 const userModel = mongoose.model('userSchema', userSchema);
 module.exports = userModel;
--- routes/authUser.js
+++ routes/authUser.js
@@ -4,6 +4,17 @@ const authUser = express.Router();
 const userModel = require('../models/User');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = require('../config/keys').JWT_SECRET;
+const crypto = require('crypto');
+
+// email verification
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+	service: 'hotmail',
+	auth: {
+		user: 'login-app-verify@outlook.com',
+		pass: 'verifyMailer'
+	}
+});
 
 // login
 authUser.get('/login', (req, res) => {
@@ -20,6 +31,7 @@ authUser.get('/change-password', (req, res) => {
 
 authUser.post('/register', async (req, res) => {
 	const { name, surname, email, password } = req.body;
+
 	const cryptedPassword = await bcrypt.hash(password, 8);
 	let errors = [];
 	if (!name || !surname || !email || !password) {
@@ -36,20 +48,59 @@ authUser.post('/register', async (req, res) => {
 				name: name,
 				surname: surname,
 				email: email,
-				password: cryptedPassword
+				password: cryptedPassword,
+				isVerified: false,
+				emailToken: crypto.randomBytes(4).toString('hex').toUpperCase()
 			});
 
-			return res.json(newUser);
+			// e-email verification
+			const emailOptions = {
+				from: 'login-app-verify@outlook.com',
+				to: 'mpinarbasi35@gmail.com',
+				subject: 'Login-App Verification Code ',
+				html: `<h2>${newUser.name} ! Thanks for registering to login-app </h2>
+				<h3>Please verify your email to complete registration</h3> 
+				<h3> Your verification code is  : ${newUser.emailToken}</h3>`
+			};
+
+			transporter.sendMail(emailOptions, (error, info) => {
+				if (error) {
+					console.log(error);
+					return;
+				}
+				console.log('Sent : ' + info.response);
+			});
+
+			return res.redirect('/authUser/verification');
 		} catch (error) {
 			if (error.code === 11000) {
 				//  duplication error
 				return res.json({ status: 'error', error: 'The given email is already in use ' });
+			} else {
+				return res.json({ status: 'error ', error: error });
 			}
-			throw error;
 		}
 	}
 });
-
+authUser.get('/verification', async (req, res) => {
+	res.render('verification');
+});
+authUser.post('/verification', async (req, res) => {
+	try {
+		const { emailToken } = req.body;
+		console.log(emailToken);
+		const user = await userModel.findOne({ emailToken });
+		if (user) {
+			user.isVerified = true;
+			await user.save();
+			res.redirect('/authUser/login');
+		} else {
+			res.redirect('/authUser/register');
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 authUser.post('/login', async (req, res) => {
 	const { email, password } = req.body;
 
@@ -60,9 +111,11 @@ authUser.post('/login', async (req, res) => {
 		// return res.json({ status: 'error', error: 'User not found ! ' });
 	}
 	if (await bcrypt.compare(password, userFound.password)) {
-		const { name, surname } = userFound;
+		const { name, surname, isVerified } = userFound;
+		if (!isVerified) {
+			return res.json({ status: 'error', data: 'Please verify your account' });
+		}
 		const token = jwt.sign({ id: userFound._id, email: userFound.email }, JWT_SECRET);
-		localStorage.setItem('token', 'mert');
 
 		res.render('success', { name, surname, email });
 	} else {
