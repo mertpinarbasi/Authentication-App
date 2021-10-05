9acd445faf0311966abd1d31c5eefc9fa4586488
mertpinarbasi <mpinarbasi35@gmail.com>
2021-10-05 20:31:41 +0300

    Minor Changes has done on admin panel file configuration

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const admin = express.Router();
-const unverifiedUsersResult = require('../routes/controllers/admin/controller.unverified-users');
-const newUsersList = require('../routes/controllers/admin/controller.new-users-list');
+const unverifiedUsersResult = require('../routes/controllers/admin/controller.admin.unverified-users');
+const newUsersList = require('../routes/controllers/admin/controller.admin.new-users-list');
 
 admin.get('/', (req, res) => {
 	res.render('../views/admin/admin');
