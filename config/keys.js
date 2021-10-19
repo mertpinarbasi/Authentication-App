e902bf8170de6ad8ea0fa5d39e8e40c5a9d08ca4
mertpinarbasi <mpinarbasi35@gmail.com>
2021-10-19 19:13:03 +0300

    .env file has been added

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,13 @@
-const JWT_SECRET = 'asdk1m2e12m3m123alsd6fsdfasdsag1babj12k123l2.12^kmdsf';
-const MONGO_URL =
-	'mongodb+srv://mertpinarbasi:xploit35@cluster0.kmaqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const SENDGRID_API_KEY = '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';
-module.exports = {
-	MONGO_URL,
-	JWT_SECRET,
-	SENDGRID_API_KEY
-};
+const process = require('process')
+
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve('config.env'),
+});
+const config  = {
+	MONGO_URL : process.env.MONGO_URL,
+	JWT_SECRET : process.env.JWT_SECRET,
+	SENDGRID_API_KEY : process.env.SENDGRID_API_KEY
+}
+
+module.exports = config
